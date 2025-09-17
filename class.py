fd10cf094861de989689a2a338b9632d832891bf
baruques <baruquechou@gmail.com>
2025-09-17 09:46:48 -0300

    ellaborating lambdas and classes

--- class.py
+++ class.py
@@ -5,3 +5,21 @@ class Calculator():
 c = Calculator() # Instantiating the class in an object
 
 print(c.soma(1, 2))
+
+class Person():
+    def __init__(self, name, age): # Constructor method
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        return f"Person(name={self.name}, age={self.age})"
+
+people = [
+    Person("Gabriel", 30),
+    Person("Maria", 25),
+    Person("João", 40)
+]
+
+people.sort(key=lambda person: person.age) # Sorting people by age and using lambda function
+
+print(people)
